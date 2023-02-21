0188891332bd2bae2ff4d33a93c900c928f82376
chj Win7Evernote VM <chjfth@gmail.com>
2023-02-21 13:46:36 +0800

    WS_EX_xxx

--- mswin/winuser.itc.h
+++ mswin/winuser.itc.h
@@ -10,6 +10,10 @@ extern CInterpretConst VK_xxx;
 extern CInterpretConst LLKHF_xxx;
 
 extern CInterpretConst WS_xxx_toplevel;
+extern CInterpretConst WS_xxx_childwnd;
+extern CInterpretConst WS_EX_xxx;
+
+extern CInterpretConst WS_EX_xxx;
 extern CInterpretConst WS_xxx_static;
 
 } // namespace itc {
